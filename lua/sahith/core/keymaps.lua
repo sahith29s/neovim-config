94223b6258956bfde9d9079843054f9fcca66386
sahith <honey.sahith29@gmail.com>
2023-12-10 14:34:27 +0530

    updated nvim

--- lua/sahith/core/keymaps.lua
+++ lua/sahith/core/keymaps.lua
@@ -5,21 +5,41 @@ local keymap = vim.keymap -- for conciseness
 -- general keymaps
 
 keymap.set("i", "jk", "<ESC>")
-keymap.set("i", "ji", "<ESC> o")
+keymap.set("i", "kj", "<ESC>")
+keymap.set("i", "jd", "<ESC>o")
 keymap.set("i", "jf", "<ESC>")
-keymap.set("n", "<leader>r", "daw")
-keymap.set("n", "<leader>nh", ":nohl<CR>")
+keymap.set("i", "js", "<Esc>S")
+keymap.set("i", "ji", "<Esc>I")
+keymap.set("i", "ja", "<Esc>A")
+keymap.set("i", "jv", "<Esc>V")
+keymap.set("i", "j;", "<esc>v")
+keymap.set("i", "j;", "<ESC>mqA;<ESC>`qa")
+keymap.set("i", "<C-H>", "<C-W>")
 
+keymap.set("n", "<leader>r", "daw")
 keymap.set("n", "<leader>+", "<C-a>")
+keymap.set("n", "<leader>l", "<C-w>w")
 keymap.set("n", "<leader>-", "<C-a>")
 keymap.set("n", "<leader>c", "caw")
-keymap.set("n", "<leader>i", "cit")
-keymap.set("n", "<leader>s", ":w<CR>")
+keymap.set("n", "<leader>m", ":delete<cr>")
+-- keymap.set("n", "<leader>i", "cit")
+keymap.set("n", "<leader>i", "gcc")
+-- keymap.set("n", "g/", "gcc")
 
---kemap.set("n" , "<leader>sv" , "<C-w>v")
---kemap.set("n" , "<leader>sh" , "<C-w>s")
---kemap.set("n" , "<leader>se" , "<C-w>=")
---kemap.set("n" , "<leader>sx" , ":close<CR>")
+keymap.set("n", "<leader>s", "S")
+keymap.set("n", "<leader>v", "V")
+keymap.set("n", "<leader>a", "A")
+keymap.set("n", "<leader>f", "I")
+keymap.set("n", "<leader>j", "_")
+keymap.set("n", "<leader>k", "$")
+keymap.set("n", "<leader>;", "mqA;<ESC>`q")
+
+-- keymap.set("n", "<leader>s", ":w<CR>")
+keymap.set("n", "<leader>w", "<C-w>v")
+keymap.set("n", "<leader>n", "<C-w>s")
+-- keymap.set("n", "<leader>s", "<C-w>=")
+keymap.set("n", "<leader>x", ":close<CR>")
+keymap.set("n", "<leader>nh", ":nohl<CR>")
 
 -- vim maximizer
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
@@ -28,4 +48,34 @@ keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
 
 -- control backspace
--- keymap.set("i", "<C-BS>", "<ESC>caw")
+-- keymap.set("i", "<C-backspace>", "<ESC>caw")
+
+-- Toggle comments with a single key
+vim.api.nvim_set_keymap("n", "<leader>/", [[:lua ToggleComment()<CR>]], { noremap = true, silent = true })
+
+function ToggleComment()
+	local comment_symbol = "// "
+	local line = vim.fn.getline(".")
+	if string.find(line, "^" .. comment_symbol) then
+		-- Uncomment the line
+		vim.fn.setline(".", string.gsub(line, comment_symbol, ""))
+	else
+		-- Comment the line
+		vim.fn.setline(".", comment_symbol .. line)
+	end
+end
+
+-- Toggle comments with a single key
+vim.api.nvim_set_keymap("n", "g/", [[:lua ToggleComment()<CR>]], { noremap = true, silent = true })
+
+function ToggleComment()
+	local comment_symbol = "// "
+	local line = vim.fn.getline(".")
+	if string.find(line, "^" .. comment_symbol) then
+		-- Uncomment the line
+		vim.fn.setline(".", string.gsub(line, comment_symbol, ""))
+	else
+		-- Comment the line
+		vim.fn.setline(".", comment_symbol .. line)
+	end
+end
